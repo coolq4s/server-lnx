adb9bd269695b9690223e71df74461eedff92c08
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-18 04:46:01 +0700

    update zero.sh

--- zerotier/zero.sh
+++ zerotier/zero.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 clear
-alias header='cat << "EOF"
+
+cat << "EOF" > header.txt
     ______            ______
    / ____/___  ____  / / __ \____ ______
   / /   / __ \/ __ \/ / / / / __ `/ ___/
  / /___/ /_/ / /_/ / / /_/ / /_/ (__  )
  \____/\____/\____/_/\___\_\__,_/____/
                       ZEROTIER INSTALLER
-EOF'
-
-header
+EOF

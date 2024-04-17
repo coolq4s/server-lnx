ec4ebc26c7f9e2b80cfbed6d5353a3d52fe7eaaf
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-18 04:51:34 +0700

    update zero.sh

--- zerotier/zero.sh
+++ zerotier/zero.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!bin/bash
+
 clear
 
+echo -e "\e[31m"
 cat << "EOF" > header.txt
     ______            ______
    / ____/___  ____  / / __ \____ ______
@@ -9,14 +11,46 @@ cat << "EOF" > header.txt
  \____/\____/\____/_/\___\_\__,_/____/
                       ZEROTIER INSTALLER
 EOF
-
+cat header.txt
 sleep 1s
 
-cat header.txt
+echo ""
+echo ""
 
-sleep 1s
+#Mencari variabel net.ipv4.ip_forward dalam sysctl.conf
+if grep -q '^#*net.ipv4.ip_forward' /etc/sysctl.conf; then
+    #Menampilkan nilai variabel dan menanyakan untuk mengubahnya
+    current_value=$(grep '^#*net.ipv4.ip_forward' /etc/sysctl.conf)
+    echo -e "\e[97m Value net.ipv4.ip_forward is: \e[92m \n $current_value\n"
+    echo -e ""
+    echo -e " Note : If first character has # \n it is disabled, press 1 to actived"
+    echo -e "\e[97m Input new value for IPv4 Forwarding"
+    echo -e "\e[0m 0 = Disable"
+    echo -e "\e[0m 1 = Active"
+    read -p " Type : " new_value
 
-cat header.txt
+    #Menghapus tanda pagar jika ada
+    sudo sed -i '/^#*net.ipv4.ip_forward/s/^#*//g' /etc/sysctl.conf
 
-read
-rm -rf header.txt
+    #Jika variabel tidak ada di akhir file, tambahkan baris baru
+    if [ -z "$current_value" ]; then
+        echo "net.ipv4.ip_forward=$new_value" | sudo tee -a /etc/sysctl.conf > /dev/null
+    else
+        # Ubah nilai variabel sesuai dengan input pengguna
+        sudo sed -i "s/^net.ipv4.ip_forward=.*/net.ipv4.ip_forward=$new_value/g" /etc/sysctl.conf
+    fi
+    sleep 2s
+else
+    #Jika tidak ditemukan, tambahkan baris baru di akhir file
+    echo  -e "\e[31m"
+    read -p " This line net.ipv4.ip_forward not found" new_value
+    echo -e "\e[97m Input new value for IPv4 Forwarding"
+    echo -e "\e[0m 0 = Disable"
+    echo -e "\e[0m 1 = Active"
+    read -p " Type : " new_value
+    echo "net.ipv4.ip_forward=$new_value" | sudo tee -a /etc/sysctl.conf > /dev/null
+    echo " Done"
+fi
+
+# Terapkan perubahan ke kernel
+# sudo sysctl -p

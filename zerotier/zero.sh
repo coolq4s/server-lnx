8a94a3b1e86cf2d816976cc475c2703acdf22a9f
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-18 04:32:00 +0700

    update zero.sh

--- zerotier/zero.sh
+++ zerotier/zero.sh
@@ -9,7 +9,17 @@ cat << "EOF"
 EOF
 
 
-exec 3> >(cat)
+exec 3> >(
+    "EOF"
+    ______            ______
+   / ____/___  ____  / / __ \____ ______
+  / /   / __ \/ __ \/ / / / / __ `/ ___/
+ / /___/ /_/ / /_/ / / /_/ / /_/ (__  )
+ \____/\____/\____/_/\___\_\__,_/____/
+                      ZEROTIER INSTALLER
+EOF
+    
+    )
 echo "To Terminal"
 echo "To cat" 1>&3
 echo "To cat again" 1>&3

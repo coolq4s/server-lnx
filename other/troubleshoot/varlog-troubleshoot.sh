3fa558259b007d1265b218ef8fcfa198f56ea65d
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 20:53:23 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -40,7 +40,7 @@ if grep -qE 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     echo "\e[0m"
     read -p " Type number (1-4): " option_logrotate_duration
     if ! [ "$option_logrotate_duration" -ge 1 -a "$option_logrotate_duration" -le 4 ]; then
-        echo "\e[101m\e[30m Only number (1-4) can be allowed.\e[0m"
+        echo "\e[101m\e[97m Only number (1-4) can be allowed.\e[0m"
         exit 1
     fi
     case $option_logrotate_duration in

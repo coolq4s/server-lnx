3d9847597e7640c6de951150497d90e98a654a58
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 20:00:12 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -29,8 +29,8 @@ echo ""
 
 if grep -qE 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     #Menampilkan nilai variabel dan menanyakan untuk mengubahnya
-    current_value=$(grep -E 'weekly|daily|monthly|yearly' /etc/logrotate.conf)
-    echo " $current_value"
+    current_value=$(grep 'weekly|daily|monthly|yearly' /etc/logrotate.conf)
+    echo "grep -E $current_value"
     echo "\e[0m Rotate log files found"
     echo ""
 #    echo -e " Note : If first character has # \n it is disabled, press 1 to actived"

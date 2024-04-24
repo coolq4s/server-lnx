93ea922b9bea9f8c597cefa7913f233c27f5810f
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 19:02:26 +0700

    Create varlog-troubleshot.sh

---
+++ other/troubleshoot/varlog-troubleshot.sh
@@ -0,0 +1,43 @@
+if grep -q '^#*net.ipv4.ip_forward' /etc/sysctl.conf; then
+    #Menampilkan nilai variabel dan menanyakan untuk mengubahnya
+    current_value=$(grep '^#*net.ipv4.ip_forward' /etc/sysctl.conf)
+    echo -e "\e[0m Value net.ipv4.ip_forward is: \e[92m \n $current_value\n"
+    echo -e ""
+    echo -e " Note : If first character has # \n it is disabled, press 1 to actived"
+    echo -e "\e[0m Input new value for IPv4 Forwarding"
+    echo -e "\e[0m 0 = Disable"
+    echo -e "\e[0m 1 = Active"
+    new_value=""
+    while [ -z "$new_value" ]; do
+        read -p " Type : " new_value
+        if [ -z "$new_value" ]; then
+            echo -e "\e[0m\033[91m Error : Cannot blank \e[0m"
+        fi
+    done
+    #Menghapus tanda pagar jika ada
+    sudo sed -i '/^#*net.ipv4.ip_forward/s/^#*//g' /etc/sysctl.conf
+
+    #Jika variabel tidak ada di akhir file, tambahkan baris baru
+    if [ -z "$current_value" ]; then
+        echo "net.ipv4.ip_forward=$new_value" | sudo tee -a /etc/sysctl.conf > /dev/null
+    else
+        # Ubah nilai variabel sesuai dengan input pengguna
+        sudo sed -i "s/^net.ipv4.ip_forward=.*/net.ipv4.ip_forward=$new_value/g" /etc/sysctl.conf
+    fi
+    sleep 2s
+else
+    #Jika tidak ditemukan, tambahkan baris baru di akhir file
+    echo  -e "\033[91m"
+    echo -e " This line net.ipv4.ip_forward not found"
+    echo -e "\e[0m Input new value for IPv4 Forwarding"
+    echo -e "\e[0m 0 = Disable"
+    echo -e "\e[0m 1 = Active"
+    new_value=""
+    while [ -z "$new_value" ]; do
+        read -p " Type : " new_value
+        if [ -z "$new_value" ]; then
+            echo -e "\e[0m\033[91m Error : Cannot blank \e[0m"
+        fi
+    done
+    echo "net.ipv4.ip_forward=$new_value" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi

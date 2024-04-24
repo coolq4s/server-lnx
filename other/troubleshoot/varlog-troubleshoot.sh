ee3663e097a19694f4bf6a440db9b31d26572eea
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 20:38:26 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -38,7 +38,7 @@ if grep -qE 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     echo "3. Monthly"
     echo "4. Yearly"
     read -p "Choose number (1-4): " choice
-    if ! [[ "$choice" =~ ^[1-4]$ ]]; then
+    if ! [ "$choice" -ge 1 -a "$choice" -le 4 ]; then
         echo "Masukkan hanya angka antara 1 dan 4."
         exit 1
     fi

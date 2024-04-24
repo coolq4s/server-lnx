09d5d4f12ec7ce1af3263c7186a0c2cd0680438c
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 19:32:44 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -32,7 +32,7 @@ if grep -E 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     #Menampilkan nilai variabel dan menanyakan untuk mengubahnya
     current_value=$(grep 'weekly|daily|monthly|yearly' /etc/sysctl.conf)
     echo -e "\e[0m Value rotate log files: \e[92m \n $current_value\n"
-    echo -e ""
+    echo ""
 #    echo -e " Note : If first character has # \n it is disabled, press 1 to actived"
 #    echo -e "\e[0m Input new value for IPv4 Forwarding"
 #    echo -e "\e[0m 0 = Disable"

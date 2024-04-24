c811ebbd0bc0b948303d64970f2ede12bf1bd6c9
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 19:43:59 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -4,7 +4,7 @@ cleanup() {
     rm -rf server-lnx
     rm -rf varlog-troubleshoot.sh
     echo " Cleaning up temporary files"
-    echo -e " To try again this script,\n you can copy the command from github"
+    echo " To try again this script,\n you can copy the command from github"
     echo ""
 }
 

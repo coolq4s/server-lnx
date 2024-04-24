05980369c514390956e2612a9ccaba05ce335c4e
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 19:35:46 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -30,7 +30,7 @@ echo ""
 clear
 if grep -E 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     #Menampilkan nilai variabel dan menanyakan untuk mengubahnya
-    current_value=$(grep 'weekly|daily|monthly|yearly' /etc/sysctl.conf)
+    current_value=$(grep 'weekly|daily|monthly|yearly' /etc/sysctl.conf) 
     echo " \e[0m Value rotate log files: \e[92m \n $current_value\n"
     echo ""
 #    echo -e " Note : If first character has # \n it is disabled, press 1 to actived"

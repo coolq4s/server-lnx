22d4249639ee620db61c755bd98ab94726344264
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 19:33:35 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -31,7 +31,7 @@ clear
 if grep -E 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     #Menampilkan nilai variabel dan menanyakan untuk mengubahnya
     current_value=$(grep 'weekly|daily|monthly|yearly' /etc/sysctl.conf)
-    echo -e -n "\e[0m Value rotate log files: \e[92m \n $current_value\n"
+    echo -n"\e[0m Value rotate log files: \e[92m \n $current_value\n"
     echo ""
 #    echo -e " Note : If first character has # \n it is disabled, press 1 to actived"
 #    echo -e "\e[0m Input new value for IPv4 Forwarding"

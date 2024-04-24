05b88d0ee40e6e3bb1dff6dc20e46f5f87a155e0
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 20:55:49 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -44,7 +44,7 @@ if grep -qE 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     if ! [ "$option_logrotate_duration" -ge 1 -a "$option_logrotate_duration" -le 4 ]; then
         echo "\e[101m\e[97m Only number (1-4) can be allowed.\e[0m"
         read -p "Press enter to continue"
-        exit 1
+        exit
     fi
     case $option_logrotate_duration in
         1) logrotate_interval="weekly";;

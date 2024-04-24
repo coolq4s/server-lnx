4ebf4f67270acc8d0f68c0c564f5924ca14a0d3d
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 14:52:13 +0700

    update ram_cleaner.sh

--- cleaner/ram/ram_cleaner.sh
+++ cleaner/ram/ram_cleaner.sh
@@ -4,7 +4,7 @@ cleanup() {
     rm -rf ram.sh
     rm -rf server-lnx
     rm -rf header.txt
-    rm -rf ram
+    rm -rf ram_cleaner.sh
     echo " Cleaning up temporary files"
     echo -e " To use again this script,\n you can copy the command from github"
     echo ""

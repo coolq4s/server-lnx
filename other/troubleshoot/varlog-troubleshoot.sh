f55b4999dbe59df3caacc8a4f2209088294f1500
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 21:30:59 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -61,8 +61,8 @@ if grep -qE 'weekly|daily|monthly|yearly' /etc/logrotate.conf; then
     esac
 
     echo " Logrotate interval now is\e[92m $logrotate_interval\e[0m"
+    sudo sed -i 's/$current_value/$logrotate_interval/g' /etc/logrotate.conf 
     sleep 5s
-#    sudo sed -i 's/$current_value/$logrotate_interval/g' /etc/logrotate.conf 
 #    echo -e "\e[0m Input new value "
 #    echo -e "\e[0m 0 = Disable"
 #    echo -e "\e[0m 1 = Active"

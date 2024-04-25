05d2d4ef45aa4f29ca8709344b7c0a6e981a2f88
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-25 10:03:52 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -92,7 +92,7 @@ if grep -v '^[[:space:]]*#' /etc/logrotate.conf | grep -A 0 -E '^*weekly|hourly|
             log_size="${log_size}M"
             echo "$log_size"
             sudo sed -i "\$asize $log_size" /etc/logrotate.conf
-            if grep -qE '^[[:space:]]*[^#]*compress'; then
+            if grep -qE '^[[:space:]]*[^#]*compress' /etc/logrotate.conf; then
                 sudo sed -i "\$acompress" /etc/logrotate.conf
             else
                 echo "skip compress"

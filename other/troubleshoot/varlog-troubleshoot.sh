a7088226033db3144032965ab79d54c04e2a7792
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-25 09:03:42 +0700

    update varlog-troubleshoot.sh

--- other/troubleshoot/varlog-troubleshoot.sh
+++ other/troubleshoot/varlog-troubleshoot.sh
@@ -76,7 +76,7 @@ echo "\e[0m Size log you want."
 echo "\e[33m I suggest, use half from your\n total partition /var/log \e[0m"
 echo ""
 echo " Your size partition /var/log is:\e[92m $var_log_size_Human\e[0m"
-read -p " Size (in Mb): " log_size
+read -p " Size in Mb: " log_size
 if [[ "$log_size" =~ ^[0-9]+$ ]]; then
         echo "\e[101m\e[97m Only number you can enter. Force EXIT\e[0m"
         echo "$log_size"

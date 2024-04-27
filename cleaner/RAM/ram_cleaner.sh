db63500d4d58366857aaaa66bcc8423aeed222c0
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-27 13:48:44 +0700

    update ram_cleaner.sh

--- cleaner/RAM/ram_cleaner.sh
+++ cleaner/RAM/ram_cleaner.sh
@@ -57,7 +57,7 @@ if [ $totalmem -gt 1048576 ]; then
     totalmemInstalled=$(echo "scale=2; $totalmem / 1048 / 1048" | bc)
     installedMem=$(printf "%.0f" $totalmemInstalled)
     installedMem2=$(echo $installedMem GiB)
-elif [ $totalmem -gt 1048576000 ]; then
+elif [ $totalmem -lt 1048576000 ]; then
     totalmemInstalled=$(echo "scale=2; $totalmem / 1048" | bc)
     installedMem=$(printf "%.0f" $totalmemInstalled)
     installedMem2=$(echo $installedMem MiB)

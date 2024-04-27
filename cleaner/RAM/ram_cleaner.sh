000bf3e4db8090e708f529fd31c98d89171aa11b
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-27 14:52:05 +0700

    update ram_cleaner.sh

--- cleaner/RAM/ram_cleaner.sh
+++ cleaner/RAM/ram_cleaner.sh
@@ -5,6 +5,7 @@ cleanup() {
     rm -rf server-lnx
     rm -rf header.txt
     rm -rf ram_cleaner.sh
+    rm -rf LGC.sh
     echo " Cleaning up temporary files"
     echo -e " To use again this script,\n you can copy the command from github"
     echo ""
@@ -184,7 +185,7 @@ draw_progress_bar_SWAP
 wait
 echo -n "\n"
 echo -n "\n"
-echo " Press CTRL+C to clean the RAM & SWAP"
+echo " Press CTRL+C to clear RAM"
 '
 LGC
 chmod +x LGC.sh
@@ -196,153 +197,12 @@ cat header.txt
 echo -e "\e[0m"
 echo ""
 echo ""
-wait; echo "                             -PLEASE WAIT, CLEARING-"
+wait; echo "                          -PLEASE WAIT, CLEARING-"
 sudo sync && echo 3 > /proc/sys/vm/drop_caches
 clear
 
 source LGC.sh
 
-watch -n1 -tc '
-echo -n "\033[1;94m"
-cat header.txt
-echo -n "\e[0m"
-echo ""
-echo ""
-echo "                                  -AFTER CLEARING-"
-echo ""
-echo ""
-#RAM
-#Count Used Ram
-used=$(free -w | awk "NR==2 {print \$3}")
-shared=$(free -w | awk "NR==2 {print \$5}")
-buff=$(free -w | awk "NR==2 {print \$6}")
-cache=$(free -w | awk "NR==2 {print \$7}")
-
-totalMemUsed=$(($used + $shared + $buff + $cache))
-
-
-if [ $totalMemUsed -gt 1048576 ]; then
-    totaluse=$(echo "scale=2; $totalMemUsed / 1048 / 1048" | bc)
-    totalresult=$(printf "%.0f" $totaluse)
-    totalresult2=$(echo $totalresult GiB)
-else
-    totaluse=$(echo "scale=2; $totalMemUsed / 1048" | bc)
-    totalresult=$(printf "%.0f" $totaluse)
-    totalresult2=$(echo $totalresult MiB)
-fi
-
-#Count Installed RAM
-totalmem=$(free -w | awk "NR==2 {print \$2}")
-
-if [ $totalmem -gt 1048576000 ]; then
-    totalmemInstalled=$(echo "scale=2; $totalmem / 1048 / 1048" | bc)
-    installedMem=$(printf "%.0f" $totalmemInstalled)
-    installedMem2=$(echo $installedMem GiB)
-else
-    totalmemInstalled=$(echo "scale=2; $totalmem / 1048" | bc)
-    installedMem=$(printf "%.0f" $totalmemInstalled)
-    installedMem2=$(echo $installedMem MiB)
-fi
-
-#Count Free RAM
-freeRAM=$(free -w | awk "NR==2 {print \$4}")
-if [ $freeRAM -gt 1048576000 ]; then
-    totalfreeRAM=$(echo "scale=2; $freeRAM / 1048 / 1048" | bc)
-    availableRAM=$(printf "%.0f" $totalfreeRAM)
-    availableRAM2=$(echo $availableRAM GiB)
-else
-    totalfreeRAM=$(echo "scale=2; $freeRAM / 1048" | bc)
-    availableRAM=$(printf "%.0f" $totalfreeRAM)
-    availableRAM2=$(echo $availableRAM MiB)
-fi
-
-#Bar RAM
-getPercent=$(echo "scale=2; ($totalresult / $installedMem) * 100" | bc )
-percentage=$(printf "%.0f" "$getPercent")
-
-progress=$percentage
-total=100
-
-draw_progress_bar_RAM() {
-    local percent=$((progress * 100 / total))
-    local num_bar=$((percent / 4))
-    local num_space=$((25 - num_bar))
-    printf " RAM  ["
-    printf "\033[91m%0.s+\e[0m" $(seq 1 $num_bar)
-    printf "\033[92m%0.s-\e[0m" $(seq 1 $num_space)
-    printf "] %d%%\r" $percent
-    echo ""
-    printf "      \033[102m\033[30m F: $availableRAM2 \033[101m\033[30m U: $totalresult2 \e[0m T: $installedMem2"
-    echo ""
-}
-
-draw_progress_bar_RAM
-echo -n "\n"
-
-#SWAP
-used_swap=$(free -w | awk "NR==3 {print \$3}")
-free_swap=$(free -w | awk "NR==3 {print \$4}")
-total_swap=$(free -w | awk "NR==3 {print \$2}")
-
-if [ $used_swap -gt 1048576000 ]; then
-    swap_used=$(echo "scale=2; $used_swap / 1048 / 1048" | bc)
-    swapresult=$(printf "%.0f" $swap_used)
-    swapresult2=$(echo $swapresult GiB)
-else
-    swap_used=$(echo "scale=2; $used_swap / 1048" | bc)
-    swapresult=$(printf "%.0f" $swap_used)
-    swapresult2=$(echo $swapresult MiB)
-fi
-
-if [ $free_swap -gt 1048576000 ]; then
-    free_swap_count=$(echo "scale=2; $free_swap / 1048 / 1048" | bc)
-    availableSWAP=$(printf "%.0f" $free_swap_count)
-    availableSWAP2=$(echo $availableSWAP GiB)
-else
-    free_swap_count=$(echo "scale=2; $free_swap / 1048" | bc)
-    availableSWAP=$(printf "%.0f" $free_swap_count)
-    availableSWAP2=$(echo $availableSWAP MiB)
-fi
-
-
-if [ $total_swap -gt 1048576000 ]; then
-    total_swap_count=$(echo "scale=2; $total_swap / 1048 / 1048" | bc)
-    totalSWAP=$(printf "%.0f" $total_swap_count)
-    totalSWAP2=$(echo $totalSWAP GiB)
-else
-    total_swap_count=$(echo "scale=2; $total_swap / 1048" | bc)
-    totalSWAP=$(printf "%.0f" $total_swap_count)
-    totalSWAP2=$(echo $totalSWAP MiB)
-fi
-
-
-#Bar SWAP
-getswapPercent=$(echo "scale=2; ($swapresult / $totalSWAP) * 100" | bc )
-percentageswap=$(printf "%.0f" "$getswapPercent")
-
-progressSWAP=$percentageswap
-totalpercentSWAP=100
-
-draw_progress_bar_SWAP() {
-    local percentSWAP=$((progressSWAP * 100 / totalpercentSWAP))
-    local num_barSWAP=$((percentSWAP / 4))
-    local num_spaceSWAP=$((25 - num_barSWAP))
-    printf " SWAP ["
-    if [ $percentSWAP -le 0 ]; then
-        printf "\033[92m%0.s-\e[0m" $(seq 1 $num_spaceSWAP)
-    else
-        printf "\033[91m%0.s+\e[0m" $(seq 1 $num_barSWAP)
-        printf "\033[92m%0.s-\e[0m" $(seq 1 $num_spaceSWAP)
-    fi
-    printf "] %d%%\r" $percentSWAP
-    echo ""
-    printf "      \033[102m\033[30m F: $availableSWAP2 \033[101m\033[30m U: $swapresult2 \e[0m T: $totalSWAP2"
-}
-
-draw_progress_bar_SWAP
-echo -n "\n"
-echo -n "\n"
-echo " Press CTRL+C to exit this tool"
 '
 clear
 exit

226587c785dd7f86ef7e0213d9402d70937b0af9
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-28 00:22:16 +0700

    update swap_resize.sh

--- other/troubleshoot/swap_resize.sh
+++ other/troubleshoot/swap_resize.sh
@@ -34,27 +34,27 @@ SWAP_SIZE_MB=$input_swap
 SWAP_FILE="/swapfile"
 
 swap_size=$(free -h | awk "NR==3 {print \$2}")
-echo "Current SWAP size :\e[102m\e[91m $swap_size"
+echo " Current SWAP size :\e[102m\e[91m $swap_size"
 echo "\e[0m"
 
 input_swap=""
-read -p "Swap size you need (MB) : " input_swap
+read -p " Swap size you need (MB) : " input_swap
 if ! [[ $input_swap =~ ^[0-9]+$ ]]; then
-    echo "Masukkan harus berupa angka."
+    echo " Masukkan harus berupa angka."
 else
     exit
 fi
 if ((input_swap >= 1 && input_swap <= 99999)); then
-    echo "Input yang valid: $input_s"
+    echo " Input yang valid: $input_s"
 else
-    echo "Input harus berada dalam rentang antara 1 hingga 99999."
+    echo " Input harus berada dalam rentang antara 1 hingga 99999."
     exit
 fi
 
 #swap process
 if ! [[ $input_swap =~ ^[0-9]+$ ]]; then
     if grep -qF "$SWAP_FILE none swap sw 0 0" /etc/fstab; then
-        echo "Baris sudah ada dalam /etc/fstab"
+        echo " Baris sudah ada dalam /etc/fstab"
     else
         sudo swapoff $SWAP_FILE
         rm -rf $SWAP_FILE
@@ -62,8 +62,8 @@ if ! [[ $input_swap =~ ^[0-9]+$ ]]; then
         sudo chmod 600 $SWAP_FILE
         sudo mkswap $SWAP_FILE
         sudo swapon $SWAP_FILE
-        echo "$SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab >/dev/null
-        echo "Baris berhasil ditambahkan ke /etc/fstab"
+        echo " $SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab >/dev/null
+        echo " Baris berhasil ditambahkan ke /etc/fstab"
     fi
 else
     exit

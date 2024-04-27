04f4a34bb51d69cd29eb9fe280a3356eefafe6bc
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-27 17:21:24 +0700

    update ram_cleaner.sh

--- cleaner/RAM/ram_cleaner.sh
+++ cleaner/RAM/ram_cleaner.sh
@@ -219,6 +219,7 @@ cat header.txt
 echo -e "\e[0m"
 echo "                          -PLEASE WAIT, CLEARING-"
 while true; do
+    echo "                                "
     echo -ne "                            \r/ "
     sleep 0.5
     echo -ne "                            \r- "

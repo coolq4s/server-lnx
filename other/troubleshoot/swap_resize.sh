6237e9d4d50760791dd7afebd3671da2fdceef9b
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-28 01:23:55 +0700

    update swap_resize.sh

--- other/troubleshoot/swap_resize.sh
+++ other/troubleshoot/swap_resize.sh
@@ -39,35 +39,31 @@ echo "\e[0m"
 input_swap=""
 read -p " Swap size you need (MB) : " input_swap
 if (($input_swap >= 1 && $input_swap <= 99999)) 3> /dev/null; then
-    echo " Input yang valid: $input_swap"
-    sleep 5
-    exit
+    if ! [[ $input_swap =~ ^[0-9]+$ ]]; then
+        if grep -qF "$SWAP_FILE none swap sw 0 0" /etc/fstab; then
+            echo " Baris sudah ada dalam /etc/fstab"
+            sleep 2
+        else
+            sudo swapoff $SWAP_FILE
+            rm -rf $SWAP_FILE
+            sudo fallocate -l ${SWAP_SIZE_MB}M $SWAP_FILE
+            sudo chmod 600 $SWAP_FILE
+            sudo mkswap $SWAP_FILE
+            sudo swapon $SWAP_FILE
+            echo " $SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab >/dev/null
+            echo " Baris berhasil ditambahkan ke /etc/fstab"
+            sleep 2
+        fi
+    else
+        exit
+    fi
 else
-    echo " Input harus berada dalam rentang antara 1 hingga 99999."
+    echo " Only number 1-99999"
     sleep 5
     exit
 fi
 
 #swap process
-if ! [[ $input_swap =~ ^[0-9]+$ ]]; then
-    if grep -qF "$SWAP_FILE none swap sw 0 0" /etc/fstab; then
-        echo " Baris sudah ada dalam /etc/fstab"
-        sleep 2
-    else
-        sudo swapoff $SWAP_FILE
-        rm -rf $SWAP_FILE
-        sudo fallocate -l ${SWAP_SIZE_MB}M $SWAP_FILE
-        sudo chmod 600 $SWAP_FILE
-        sudo mkswap $SWAP_FILE
-        sudo swapon $SWAP_FILE
-        echo " $SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab >/dev/null
-        echo " Baris berhasil ditambahkan ke /etc/fstab"
-        sleep 2
-    fi
-    
-else
-    exit
-fi
 
 #echo "$SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab
 

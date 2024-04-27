96228356d6227f2476fde765b51c93d584d03491
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-27 11:46:28 +0700

    update ram_cleaner.sh

--- cleaner/RAM/ram_cleaner.sh
+++ cleaner/RAM/ram_cleaner.sh
@@ -84,19 +84,8 @@ terminal_width=$(tput cols)
 max_bar_length=$((terminal_width / 4))
 
 draw_progress_bar_RAM() {
-    local percent=$(echo "$progress * 100 / $total" | bc)
-    #local num_bar=$((percent / 4))
-    
-    if [ $(echo "$percent > 100" | bc) -eq 1 ]; then
-        percent=100
-    elif [ $(echo "$percent < 0" | bc) -eq 1 ]; then
-        percent=0
-    fi
-    
+    local percent=$(($progress * 100 / $total))
     local num_bar=$((percent / 4))
-
-
-
     if [ $num_bar -gt $max_bar_length ]; then
         num_bar=$max_bar_length
     fi

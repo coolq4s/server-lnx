68c255f44b89bcb9fe5606f244cac9851a42f02f
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-27 22:57:57 +0700

    update swap_resize.sh

--- other/troubleshoot/swap_resize.sh
+++ other/troubleshoot/swap_resize.sh
@@ -13,7 +13,7 @@ cleanup() {
 trap cleanup EXIT
 trap cleanup SIGINT
 trap cleanup SIGQUIT
-while true; do
+
 clear
 echo "\e[96m"
 cat << "EOF" > header.txt
@@ -62,4 +62,3 @@ echo "$SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab
 
 sleep 2
 exit
-done

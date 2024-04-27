ce537f6bb60afaab22d7e884904c5d107cd0c002
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-27 13:40:56 +0700

    update ram_cleaner.sh

--- cleaner/RAM/ram_cleaner.sh
+++ cleaner/RAM/ram_cleaner.sh
@@ -129,7 +129,7 @@ fi
 if [ $free_swap -gt 1048576 ]; then
     free_swap_count=$(echo "scale=2; $free_swap / 1048 / 1048" | bc)
     availableSWAP=$(printf "%.0" $free_swap_count)
-    availableSWAP2=$(echo $free_swap_count GiB)
+    availableSWAP2=$(echo "$free_swap_count GiB")
 elif [ $free_swap -lt 1048576000 ]; then
     free_swap_count=$(echo "scale=2; $free_swap / 1048" | bc)
     availableSWAP=$(printf "%.0f" $free_swap_count)

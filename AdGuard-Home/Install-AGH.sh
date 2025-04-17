94527a678898142fa32a77d658a6d29cba907f5b
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2025-04-18 00:50:53 +0700

    Update Install-AGH.sh

--- AdGuard-Home/Install-AGH.sh
+++ AdGuard-Home/Install-AGH.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+
 cleanup() {
     clear
-    rm -rf header.txt
+    rm -f header.txt
     rm -rf server-lnx
-    rm -rf Install-AGH.sh
+    rm -f Install-AGH.sh
     echo ""
     echo " Cleaning up temporary files"
-    echo " To try again this script,\n you can copy the command from github"
+    echo " To try again this script, you can copy the command from github"
     echo ""
 }
 
@@ -14,8 +15,8 @@ trap cleanup EXIT
 
 clear
 
-echo "\e[92m"
-cat << "EOF" > header.txt
+# Membuat header.txt dengan format yang benar
+cat > header.txt << 'EOF'
     ______            ______
    / ____/___  ____  / / __ \____ ______
   / /   / __ \/ __ \/ / / / / __ `/ ___/
@@ -23,39 +24,43 @@ cat << "EOF" > header.txt
  \____/\____/\____/_/\___\_\__,_/____/
                            AGH INSTALLER
 EOF
-cat header.txt
-sleep 1s
 
-echo ""
-echo ""
+# Menampilkan header dengan warna hijau (hanya jika menggunakan bash)
+printf "\033[92m"
+cat header.txt
+printf "\033[0m"  # Reset warna
+
+sleep 1
+
+echo "\n"
 
 wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm64.tar.gz
 clear
-echo "\e[92m"
+printf "\033[92m"
 cat header.txt
-sleep 1s
-echo ""
-echo ""
+printf "\033[0m"
+sleep 1
+echo "\n"
 echo "Extracting AGH Package ..."
-sleep 1s
+sleep 1
 tar xvf AdGuardHome_linux_arm64.tar.gz
 echo "Extracted"
-cd AdGuardHome/
+cd AdGuardHome/ || exit
 clear
-echo "\e[92m"
+printf "\033[92m"
 cat header.txt
-sleep 1s
-echo ""
-echo ""
+printf "\033[0m"
+sleep 1
+echo "\n"
 echo "Installing AGH Component"
-sleep 1s
+sleep 1
 ./AdGuardHome -s install
 clear
-echo "\e[92m"
+printf "\033[92m"
 cat header.txt
-sleep 1s
-echo ""
-echo ""
+printf "\033[0m"
+sleep 1
+echo "\n"
 echo "Installing Done"
-sleep 1s
-exit
+sleep 1
+exit 0

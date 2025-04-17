a368ac6de82c36c3cfe4b8b90ae3c08cb46edaef
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2025-04-18 01:01:29 +0700

    Update Install-AGH.sh

--- AdGuard-Home/Install-AGH.sh
+++ AdGuard-Home/Install-AGH.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
-
-# Gunakan bash untuk fitur yang lebih reliable
-
+#!/bin/sh
 cleanup() {
     clear
-    rm -f header.txt
-    rm -f Install-AGH.sh
-    rm -rf AdGuardHome
-    rm -f AdGuardHome_linux_arm64.tar.gz
-    
-    echo -e "\nCleaning up temporary files"
-    echo "To try again this script, you can copy the command from github"
+    rm -rf "header.txt"
+    rm -rf server-lnx
+    rm -rf Install-AGH.sh
+    echo ""
+    echo " Cleaning up temporary files"
+    echo " To try again this script,\n you can copy the command from github"
     echo ""
 }
 
@@ -18,63 +14,48 @@ trap cleanup EXIT
 
 clear
 
-# Pastikan direktori kerja writable
-WORKDIR=$(dirname "$0")
-cd "$WORKDIR" || { echo "Failed to change directory"; exit 1; }
-
-# Buat header.txt dengan cara yang lebih reliable
-cat > header.txt <<'HEADER_EOF'
+echo "\e[92m"
+cat << "EOF" > header.txt
     ______            ______
    / ____/___  ____  / / __ \____ ______
   / /   / __ \/ __ \/ / / / / __ `/ ___/
  / /___/ /_/ / /_/ / / /_/ / /_/ (__  )
  \____/\____/\____/_/\___\_\__,_/____/
                            AGH INSTALLER
-HEADER_EOF
+EOF
+cat "header.txt"
+sleep 1s
 
-# Verifikasi file berhasil dibuat
-if [ ! -f "header.txt" ]; then
-    echo "Error: Failed to create header file!"
-    exit 1
-fi
+echo ""
+echo ""
 
-# Tampilkan header dengan warna
-echo -e "\e[92m"
-cat header.txt
-echo -e "\e[0m"
-
-sleep 1
-
-echo -e "\nDownloading AGH package..."
-if ! wget -q --show-progress https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm64.tar.gz; then
-    echo "Download failed!"
-    exit 1
-fi
-
+wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm64.tar.gz
 clear
-echo -e "\e[92m"
-cat header.txt
-echo -e "\e[0m"
-
-echo -e "\nExtracting AGH Package..."
-tar xvf AdGuardHome_linux_arm64.tar.gz || { echo "Extraction failed!"; exit 1; }
-echo "Extracted successfully"
-
-cd AdGuardHome/ || { echo "Cannot enter AdGuardHome directory"; exit 1; }
-
+echo "\e[92m"
+cat "header.txt"
+sleep 1s
+echo ""
+echo ""
+echo "Extracting AGH Package ..."
+sleep 1s
+tar xvf AdGuardHome_linux_arm64.tar.gz
+echo "Extracted"
+cd AdGuardHome/
 clear
-echo -e "\e[92m"
-cat header.txt
-echo -e "\e[0m"
-
-echo -e "\nInstalling AGH Component..."
-./AdGuardHome -s install || { echo "Installation failed!"; exit 1; }
-
+echo "\e[92m"
+cat "header.txt"
+sleep 1s
+echo ""
+echo ""
+echo "Installing AGH Component"
+sleep 1s
+./AdGuardHome -s install
 clear
-echo -e "\e[92m"
-cat header.txt
-echo -e "\e[0m"
-
-echo -e "\nInstallation Done!"
-sleep 2
+echo "\e[92m"
+cat "header.txt"
+sleep 1s
+echo ""
+echo ""
+echo "Installing Done"
+sleep 1s
 exit
